44a608a59a7d32997f548ba4ed1e9f36c16a32f2
BalanM14 <127307497+vasanthabalanm@users.noreply.github.com>
2023-08-01 00:02:56 +0530

    Data Context Created

---
+++ MakeMyTrip/MakeMyTrip/Data/MakeTripContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Numerics;
+using System.Reflection.Emit;
+
+namespace MakeMyTrip.Data
+{
+    public class MakeTripContext : DbContext
+    {
+        public MakeTripContext(DbContextOptions<MakeTripContext> options) : base(options) { }
+
+        public DbSet<Admin> Admins { get; set; }
+        public DbSet<Doctor> Doctors { get; set; }
+        public DbSet<Patient> Patients { get; set; }
+        public DbSet<Appointment> Appointments { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+
+    }
+}
