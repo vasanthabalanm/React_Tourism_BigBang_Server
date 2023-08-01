2fc6a002bf075846ad758d7c746fca18784d6788
BalanM14 <127307497+vasanthabalanm@users.noreply.github.com>
2023-08-01 13:27:05 +0530

    Model updated

--- MakeMyTrip/MakeMyTrip/Data/MakeTripContext.cs
+++ MakeMyTrip/MakeMyTrip/Data/MakeTripContext.cs
@@ -1,6 +1,24 @@
-﻿namespace MakeMyTrip.Data
+﻿using MakeMyTrip.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Numerics;
+
+namespace MakeMyTrip.Data
 {
-    public class MakeTripContext
+    public class MakeTripContext : DbContext
     {
+        public MakeTripContext(DbContextOptions<MakeTripContext> options) : base(options) { }
+        public DbSet<Admin_User> Admin_s { get; set; }
+        public DbSet<Book> Books { get; set; }
+        public DbSet<Hotel> Hotel { get; set; }
+        public DbSet<PackageOffering> PackageOffering { get; set; }
+        public DbSet<Specialty> Specialty { get; set; }
+        public DbSet<Spot> Spot { get; set; }
+        public DbSet<Transaction> Transaction { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+
     }
 }
