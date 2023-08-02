bf48766e993ccc0030f6ebdf16b5d456833c520b
BalanM14 <127307497+vasanthabalanm@users.noreply.github.com>
2023-08-02 21:40:17 +0530

    controller updated

--- MakeMyTrip/MakeMyTrip/Controllers/AdminGalleryController.cs
+++ MakeMyTrip/MakeMyTrip/Controllers/AdminGalleryController.cs
@@ -1,7 +1,8 @@
-﻿using MakeMyTrip.Models;
-using MakeMyTrip.Repository.AdminAllImages;
+﻿using MakeMyTrip.Data;
+using MakeMyTrip.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace MakeMyTrip.Controllers
 {
@@ -9,29 +10,125 @@ namespace MakeMyTrip.Controllers
     [ApiController]
     public class AdminGalleryController : ControllerBase
     {
-        private readonly IAdminallImages _gallery;
+        private readonly MakeTripContext _context;
+        private readonly IWebHostEnvironment _hostEnvironment;
 
-        public AdminGalleryController(IAdminallImages gallery)
+
+        public AdminGalleryController(MakeTripContext context, IWebHostEnvironment hostEnvironment)
         {
-            _gallery = gallery;
+            _context = context;
+            _hostEnvironment = hostEnvironment;
         }
 
-        //to post
+        //to post images
         [HttpPost]
-        public async Task<ActionResult<AdminImageGallery>> AddImages([FromForm] AdminImageGallery admngalry)
+        public async Task<ActionResult<AdminImageGallery>> AddImages([FromForm] AdminImageGallery imageGallery)
         {
+            imageGallery.ImageName = await SaveImage(imageGallery.ImageFile);
+            _context.AdminImagecheck.Add(imageGallery);
+            await _context.SaveChangesAsync();
+
+            return StatusCode(201);
+        }
+
+        //to save image
+        [NonAction]
+        public async Task<string> SaveImage(IFormFile imageFile)
+        {
+            string imageName = new String(Path.GetFileNameWithoutExtension(imageFile.FileName).Take(10).ToArray()).Replace(' ', '-');
+            imageName = imageName + DateTime.Now.ToString("yymmssfff") + Path.GetExtension(imageFile.FileName);
+            var imagePath = Path.Combine(_hostEnvironment.ContentRootPath, "Images/AdminListGallery", imageName);
+            using (var fileStream = new FileStream(imagePath, FileMode.Create))
+            {
+                await imageFile.CopyToAsync(fileStream);
+            }
+            return imageName;
+        }
+
+        //to get all images
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<AdminImageGallery>>> GetAllImages()
+        {
+            return await _context.AdminImagecheck
+                .Select(x => new AdminImageGallery()
+                {
+                    AdminImgsId = x.AdminImgsId,
+                    LocationName = x.LocationName,
+                    Locationdescription = x.Locationdescription,
+                    ImageName = x.ImageName,
+                    ImageSrc = String.Format("{0}://{1}{2}/Images/{3}", Request.Scheme, Request.Host, Request.PathBase, x.ImageName)
+                })
+                .ToListAsync();
+        }
+
+        //to update the images with details
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutAdminImages(int id, [FromForm] AdminImageGallery imageGallery)
+        {
+            if (id != imageGallery.AdminImgsId)
+            {
+                return BadRequest();
+            }
+
+            if (imageGallery.ImageFile != null)
+            {
+                DeleteImage(imageGallery.ImageName);
+                imageGallery.ImageName = await SaveImage(imageGallery.ImageFile);
+            }
+
+            _context.Entry(imageGallery).State = EntityState.Modified;
+
             try
             {
-
-                return StatusCode(201);
+                await _context.SaveChangesAsync();
             }
-            catch (Exception ex)
+            catch (DbUpdateConcurrencyException)
             {
-                // Log the exception or handle it appropriately.
-                // You can return a custom error response if needed.
-                return StatusCode(500, "An error occurred while processing the request.");
+                if (!ImageExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
             }
+
+            return NoContent();
         }
+
+        //to check catch error meathod
+        private bool ImageExists(int id)
+        {
+            return _context.AdminImagecheck.Any(e => e.AdminImgsId == id);
+        }
+
+        //to delete images
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<AdminImageGallery>> DeleteImage(int id)
+        {
+            var imageGallery = await _context.AdminImagecheck.FindAsync(id);
+            if (imageGallery == null)
+            {
+                return NotFound();
+            }
+            DeleteImage(imageGallery.ImageName);
+            _context.AdminImagecheck.Remove(imageGallery);
+            await _context.SaveChangesAsync();
+
+            return imageGallery;
+        }
+
+        //delete image meathod
+        [NonAction]
+        public void DeleteImage(string imageName)
+        {
+            var imagePath = Path.Combine(_hostEnvironment.ContentRootPath, "Images/AdminListGallery", imageName);
+            if (System.IO.File.Exists(imagePath))
+                System.IO.File.Delete(imagePath);
+        }
+
+
 
     }
 }
