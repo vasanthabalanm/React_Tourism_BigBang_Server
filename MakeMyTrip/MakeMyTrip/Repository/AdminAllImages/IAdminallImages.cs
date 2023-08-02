dbfc92896c1e859a1c62b6c09a73d3dcc1349fb9
BalanM14 <127307497+vasanthabalanm@users.noreply.github.com>
2023-08-02 19:51:35 +0530

    AdminImage repository updated

--- MakeMyTrip/MakeMyTrip/Repository/AdminAllImages/IAdminallImages.cs
+++ MakeMyTrip/MakeMyTrip/Repository/AdminAllImages/IAdminallImages.cs
@@ -1,6 +1,13 @@
-﻿namespace MakeMyTrip.Repository.AdminAllImages
+﻿using MakeMyTrip.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MakeMyTrip.Repository.AdminAllImages
 {
     public interface IAdminallImages
     {
+        Task<ActionResult<AdminImageGallery>> AddImages(AdminImageGallery imageGallery);
+        Task<IEnumerable<AdminImageGallery>> GetAllImages(HttpRequest request);
+        Task<IActionResult> UpdateEmployee(int id, AdminImageGallery imageGallery);
+        Task<ActionResult<AdminImageGallery>> DeleteImage(int id);
     }
 }
